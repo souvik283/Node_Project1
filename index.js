5c8b640606f0ffbc61e862740e8b59941b2f199f
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-26 18:58:55 +0530

    practice changes

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require("express")
+const fs = require("fs")
 const users = require("./MOCK_DATA.json")
 
 const app = express();
-let port=4000;
+let port = 4000;
 
-app.get("/api/users", (req, res)=>{
+app.use(express.urlencoded({extended :false}))
+
+app.get("/api/users", (req, res) => {
     return res.json(users);
 })
 
-app.get("/users", (req, res)=>{
+app.get("/users", (req, res) => {
     let html = `
     <ul>
-    ${users.map((user)=>`<li> ${user.first_name} ${user.last_name} </li>`).join("")}
+    ${users.map((user) => `<li> ${user.first_name} ${user.last_name} </li>`).join("")}
     </ul>
     `
     res.send(html)
 })
 
-app.get("/users/:id", (req, res)=>{
-    const id2= Number(req.params.id)
-    const user = users.find((user)=> user.id === id2)
+app.get("/users/:id", (req, res) => {
+    const id2 = Number(req.params.id)
+    const user = users.find((user) => user.id === id2)
     let inhtml = ` <h2>
     ${user.first_name} ${user.last_name} is the user having userId: ${user.id}. His/Her number is: ${user.mobile_no}
     </h2>`
@@ -27,23 +30,27 @@ app.get("/users/:id", (req, res)=>{
 })
 
 app.route("/api/users/:id")
-.get((req, res)=>{
-    let id1 = Number(req.params.id)
-    console.log(id1)
-    const user1 = users.find((user)=> user.id === id1)
+    .get((req, res) => {
+        let id1 = Number(req.params.id)
+        // console.log(id1)
+        const user1 = users.find((user) => user.id === id1)
 
-    return res.json(user1)
-}).patch((req, res)=>{
-    return res.json({ staus : "pending"})
-}).delete((req, res)=>{
-    return res.json({ staus : "pending"})
+        return res.json(user1)
+    }).patch((req, res) => {
+        return res.json({ staus: "pending" })
+    }).delete((req, res) => {
+        return res.json({ staus: "pending" })
+    })
+
+app.post("/api/users", (req, res) => {
+    const body = req.body
+    users.push({ ...body, id: users.length + 1 })
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+        return res.json({ staus: "successful", id: users.length })
+    })
 })
 
-app.post("/api/users", (req, res)=>{
-    return res.json({ staus : "pending"})
-})
 
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server started at port ${port}`)
 })
